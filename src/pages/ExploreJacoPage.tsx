4284dd0ed96c6c003117dea286e4bb6e8c22c595
sohan-n <68312924+sohan-n@users.noreply.github.com>
2025-07-07 12:11:36 -0400

    alignment of title

--- src/pages/ExploreJacoPage.tsx
+++ src/pages/ExploreJacoPage.tsx
@@ -91,6 +91,7 @@ const JacoPage: React.FC = () => {
             initial={{ opacity: 0, y: 40 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 1.1, delay: 0.5 }}
+            style={{ marginTop: '10%' }}
           >
             <Typography
               variant="h2"
