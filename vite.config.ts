f726abd4c9d632e7cb48c29ff8fba32a24258e8c
sohan-n <68312924+sohan-n@users.noreply.github.com>
2025-07-08 13:00:29 -0400

    FIXED :)

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => ({
+export default defineConfig({
   plugins: [react()],
-  base: command === 'serve' ? '/' : '/condos_arnold/',
+  base: '/',
   build: {
     rollupOptions: {
       input: {
@@ -13,4 +13,4 @@ export default defineConfig(({ command }) => ({
       },
     },
   },
-}))
+})
